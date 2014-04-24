9e6c71f297022ce904e0fe707f9ceb1a95857473
jxb532 <jxb532@case.edu>
2014-04-24 19:20:23 -0400

    added build script

--- src/buildProbGame.sh
+++
@@ -1,2 +0,0 @@
-javac -cp "Sepia.jar" ProbAgent.java GameBoard.java Node.java Pair.java
-java -cp "Sepia.jar;." edu.cwru.sepia.Main2 probConfig.xml
---
+++ src/buildQLearningGame2.sh
@@ -0,0 +1,2 @@
+javac -cp "Sepia.jar" QLearningAgent.java Pair.java
+java -cp "Sepia.jar;." edu.cwru.sepia.Main2 CombatConfig2.xml
